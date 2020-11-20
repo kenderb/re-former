f6a035df9b08648b1d9b5ad20552a6398702f186
julian3493 <julianramosarevalo@gmail.com>
2020-11-20 11:24:55 -0500

    add destroy and update methods in user controller

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'sass-rails', '6.0.0'
 gem 'turbolinks', '5.2.1'
 gem 'webpacker', '4.2.2'
 gem 'will_paginate', '3.3.0'
+gem 'tzinfo-data'
 
 group :development, :test do
   gem 'byebug', '11.1.3', platforms: %i[mri mingw x64_mingw]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was succesfully destroy'}
+      format.json { head :no_content}
+    end
+  end
+
   private
 
   def set_user
