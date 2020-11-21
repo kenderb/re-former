89a773ac243078b3fcc36d07f0d98d3b0f8db1dd
kenderb <kenderbolivar@gmail.com>
2020-11-20 20:42:10 -0500

    remove show and edit form the resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'users#index', as: 'users_index'
-  resources :users, only: [:index, :new, :show, :create, :edit, :update, :destroy]
+  resources :users, only: [:index, :new, :create, :update, :destroy]
 end
